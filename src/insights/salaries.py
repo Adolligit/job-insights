fe0f7f09d2a973be47c89ac5a3b31674426aad46
Adolligit <adelson.onod@gmail.com>
2022-12-11 21:08:43 -0300

    feat: (get_min_salary) foi criada a função para pegar o menor salário da coluna 'min_salary'

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -39,7 +39,11 @@ def get_min_salary(path: str) -> int:
     int
         The minimum salary paid out of all job opportunities
     """
-    raise NotImplementedError
+    return min([
+        int(job["min_salary"])
+        for job in read(path)
+        if job["min_salary"].isdigit()
+    ])
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
