05c362bceba0132a7964c7e5017f843a311ace18
Adolligit <adelson.onod@gmail.com>
2022-12-11 21:45:23 -0300

    feat: (filter_by_industry) filtrando empregos pela indústria

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -41,4 +41,8 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    return [
+        job
+        for job in jobs
+        if industry == job['industry']
+    ]
