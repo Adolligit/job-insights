e66a7eb1d0c7fea0bbdbbe7f8c0e3611f68ac47f
Adolligit <adelson.onod@gmail.com>
2022-12-14 21:33:22 -0300

    test: (count_ocurrences) criando teste unitário para a função

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
     pass
+    assert count_ocurrences("data/jobs.csv", "job") == 3454
+    assert count_ocurrences("data/jobs.csv", "New York") == 597
+    assert count_ocurrences("data/jobs.csv", "369") == 10
